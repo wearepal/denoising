d571560de6ba62b0ed45c34255c94d72cb7f9a8a
stoffle <stoffle@users.noreply.github.com>
2018-11-06 13:32:42 +0000

    Switched to pathlib.Path for file paths

--- utils/loader.py
+++ utils/loader.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import pandas as pd
 from skimage import io
 from torch.utils.data import Dataset
@@ -10,19 +10,15 @@ class HuaweiDataset(Dataset):
             root_dir (string, optional): Directory with all the image folders, and 'Training_Info.csv'.
             transform (callable, optional): Optional transform to be applied on a sample.
         """
-        self.root_dir = root_dir
         self.transform = transform
         if root_dir is not None:
-            self.root_dir = root_dir
+            self.root_dir = Path(root_dir).resolve()
         else:
             # Use the default path, assumes repo was cloned alongside a `data` folder
-            self.root_dir = os.path.join(
-                os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-                "data"
-            )
-        if not os.path.isdir(self.root_dir):
+            self.root_dir = Path(__file__).resolve().parent.parent.parent/"data"
+        if not self.root_dir.is_dir():
             raise ValueError("No valid top directory specified")
-        self.info_df = pd.read_csv(os.path.join(self.root_dir, "Training_Info.csv"))
+        self.info_df = pd.read_csv(self.root_dir/"Training_Info.csv")
 
     def __len__(self):
         return len(self.info_df)
@@ -30,14 +26,14 @@ class HuaweiDataset(Dataset):
     def __getitem__(self, idx):
         if idx not in range(len(self.info_df)):
             raise IndexError
-        clean_location, noisy_location = self._get_image_names(idx)
+        clean_location, noisy_location = self._get_image_locations(idx)
         clean_image = io.imread(clean_location)
         noisy_image = io.imread(noisy_location)
         ISO = self.info_df.iloc[idx]['ISO_Info']
         sample = {
             'clean': clean_image,
             'noisy': noisy_image,
-            'iso': ISO
+            'iso': ISO,
         }
 
         if self.transform is not None:
@@ -45,7 +41,7 @@ class HuaweiDataset(Dataset):
 
         return sample
     
-    def _get_image_names(self, idx):
+    def _get_image_locations(self, idx):
         class_info = self.info_df.iloc[idx]['Class_Info']
         # Class_Info in the csv doesn't match the directory names, so they need fixing:
         if class_info == "building":
@@ -53,6 +49,6 @@ class HuaweiDataset(Dataset):
         else:
             class_dir = class_info.capitalize()
         file_name = self.info_df.iloc[idx]['Name_Info'].capitalize()
-        clean_location = os.path.join(self.root_dir, class_dir, "Clean", file_name)
-        noisy_location = os.path.join(self.root_dir, class_dir, "Noisy", file_name)
+        clean_location = self.root_dir/class_dir/"Clean"/file_name
+        noisy_location = self.root_dir/class_dir/"Noisy"/file_name
         return clean_location, noisy_location
