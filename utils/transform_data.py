7e625b8460b4225e77ddeda99cd53df26f2edadb
stoffle <stoffle@users.noreply.github.com>
2018-11-08 11:17:30 +0000

    Added optional target directory and progress bar

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -3,23 +3,30 @@ from pathlib import Path
 import csv
 from random import seed
 from torchvision import transforms
+from tqdm import tqdm
 
-def main(patches, patch_size, new_path=None):
+def main(patches, patch_size, old_path=None, new_path=None):
     rand_crop = transforms.RandomCrop(size=patch_size)
     transform = transforms.Compose([rand_crop])
 
-    if new_path is not None:
-        root_path = Path(new_path).resolve()
+    if old_path is not None:
+        root_path = Path(old_path).resolve()
     else:
         # Use the default path, assumes repo was cloned alongside a `data` folder
         root_path = Path(__file__).resolve().parent.parent.parent / "data"
     if not root_path.is_dir():
-        raise ValueError("No valid top directory specified")
-    transformed_path = root_path / "transformed"
+        raise ValueError("No valid data directory specified")
+
+    if new_path is not None:
+        transformed_path = Path(new_path).resolve()
+    else:
+        # Adds transformed folder to existing data dir
+        transformed_path = root_path / "transformed"
+    
     transformed_path.mkdir()
     data = HuaweiDataset(root_dir=root_path)
     iso_data = [("folder_idx", "iso")]
-    for image_no, pair in enumerate(data):
+    for image_no, pair in enumerate(tqdm(data)):
         iso_data.append((image_no, pair['iso']))
         clean_path = transformed_path / str(image_no) / "clean"
         clean_path.mkdir(parents=True)
@@ -44,6 +51,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate transformed images")
     parser.add_argument("crops", metavar="c", type=int, help="Number of random crops per image")
     parser.add_argument("size", metavar="s", type=int, help="Dimension of cropped image")
-    parser.add_argument("--data-path", dest="path", default=None, help="Data folder path") 
+    parser.add_argument("--data-path", dest="old_path", default=None, help="Data folder path")
+    parser.add_argument("--new-path", dest="new_path", default=None, help="Folder to store transformed data, must not exist yet")
     args = parser.parse_args()
-    main(args.crops, args.size, new_path=args.path)
+    main(args.crops, args.size,old_path=args.old_path , new_path=args.new_path)
