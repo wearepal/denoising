75fa0a28c37cdc52f9d2ea1181326f0dc72713bb
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-11-12 16:27:01 +0000

    Add loader that just takes the files from a CSV file

--- utils/loader.py
+++ utils/loader.py
@@ -2,7 +2,9 @@
 from pathlib import Path
 import pandas as pd
 from PIL import Image
+import torch
 from torch.utils.data import Dataset, Subset
+from torchvision import transforms
 import numpy as np
 
 class TransformedHuaweiDataset(Dataset):
@@ -122,3 +124,40 @@ class HuaweiDataset(Dataset):
         clean_location = self.root_dir / class_dir / "Clean" / file_name
         noisy_location = self.root_dir / class_dir / "Noisy" / file_name
         return clean_location, noisy_location
+
+
+class CsvLoader(Dataset):
+    """Load dataset with information from CSV file"""
+    def __init__(self, csv_path):
+        """
+        Args:
+            csv_path: (string) path to the CSV file that contains the paths to the images
+        """
+        full_path = Path(csv_path)
+        self.root_path = full_path.parent
+        self.info_df = pd.read_csv(full_path)
+
+    def __len__(self):
+        return len(self.info_df)
+
+    def __getitem__(self, idx):
+        if idx > len(self.info_df):
+            raise IndexError
+        noisy_location = self.root_path / self.info_df.iloc[idx]['noisy_path']
+        clean_location = self.root_path / self.info_df.iloc[idx]['clean_path']
+        clean_image = Image.open(clean_location)
+        noisy_image = Image.open(noisy_location)
+        return dict(
+            clean=transforms.functional.to_tensor(clean_image),
+            noisy=transforms.functional.to_tensor(noisy_image),
+            iso=torch.tensor(self.info_df.iloc[idx]['iso'], dtype=torch.float32),
+        )
+
+    def random_split(self, test_ratio=0.5, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        n_total = self.__len__()
+        n_test_images = int(n_total * test_ratio)
+        test_idx = np.random.choice(np.arange(n_total), n_test_images, replace=False)
+        train_idx = np.setdiff1d(np.arange(n_total), test_idx)
+        return Subset(self, train_idx), Subset(self, test_idx)
