1c4e43b3b2ca222b23814bfbd234de17236a76da
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-11-14 13:21:39 +0000

    Add script that loads configuration from JSON file and runs it

--- run_configs/run_common.py
+++ run_configs/run_common.py
@@ -10,6 +10,12 @@ def run(args: dict):
     """Run the pytorch model with the configuration given in args"""
     # convert the dictionary into commandline args
     arg_list = []
-    for k, v in args.items():
-        arg_list += [f"--{k}", str(v)]
+    for key, value in args.items():
+        if value is None:
+            continue
+        if isinstance(value, bool):
+            if not value:
+                arg_list += [f"--no_{key}"]
+            continue
+        arg_list += [f"--{key}", str(value)]
     main(parse_arguments(arg_list))
