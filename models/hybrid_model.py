c4a575d714276e78a5b0ee2c207afd013c883203
mb715 <mb715@sussex.ac.uk>
2018-11-16 15:46:43 +0000

    Removed test code.

--- models/hybrid_model.py
+++ models/hybrid_model.py
@@ -31,7 +31,8 @@ class HybridGatedCNN(nn.Module):
         for i in range(stream_length):
             dilation = 2 ** (i + 1)  # double dilation factor each layer
             complex_layer = ComplexGatedConvLayer(num_complex_filters, num_complex_filters,
-                                                  local_condition=args.iso, dilation=dilation)
+                                                  local_condition=args.iso, dilation=dilation,
+                                                  preserve_size=True)
             complex_layers.append(complex_layer)
 
         # Combine outputs of complex and real streams
@@ -52,7 +53,7 @@ class HybridGatedCNN(nn.Module):
         real_out = x
         for r_layer in self.real_stream:
             real_out = r_layer(real_out, c)
-            print(real_out.shape)
+
         # Complex stream
         complex_out = torch.rfft(x, signal_ndim=2)
         for c_layer in self.complex_stream:
@@ -65,100 +66,7 @@ class HybridGatedCNN(nn.Module):
         out = self.pooling_conv(cat_streams, c)
         out = self.output_conv(out, c)
 
-        print(out.shape)
         if self.residual:   # learn noise residual
             out = out + x
 
         return out
-
-
-import argparse
-
-
-def parse_arguments(raw_args=None):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-t', '--run_on_test', nargs='+',
-                        help='Evaluate a model (checkpoint) on test set. '
-                        'Args: Checkpoint path, Test data path[, Save path]')
-
-    parser.add_argument('-dd', '--data_dir', help='location of transformed data')
-    parser.add_argument('-ts', '--test_split', help='Fraction of data to be used for validation',
-                        default=0.2, type=float)
-    parser.add_argument('-ds', '--data_subset', help='Fraction of crops per image to be used',
-                        default=1.0, type=float)
-
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-
-    parser.add_argument('-nc', '--no_cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-
-    parser.add_argument('--manual_seed', type=int,
-                        help='manual seed, if not given resorts to random seed.')
-
-    parser.add_argument('-sd', '--save_dir', type=str, metavar='PATH', default='',
-                        help='path to save results and checkpoints to '
-                             '(default: ../results/<model>/<current timestamp>)')
-    parser.add_argument('--num-samples-to-log', type=int, metavar='N', default=32,
-                        help='number of image samples to write to tensorboard each epoch'
-                             ' (default: 32)')
-
-    # training parameters
-    parser.add_argument('--epochs', default=100, type=int, metavar='N',
-                        help='number of total epochs to run (default: 100)')
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='manual epoch number (useful on restarts)')
-
-    parser.add_argument('-trb', '--train_batch_size', default=256, type=int,
-                        metavar='N', help='mini-batch size for training data (default: 256)')
-    parser.add_argument('-teb', '--test_batch_size', default=256, type=int,
-                        metavar='N', help='mini-batch size for test data (default: 256)')
-
-    parser.add_argument('-lr', '--learning_rate', default=0.005, type=float,
-                        metavar='LR', help='initial learning rate (default: 0.005)')
-
-    # model parameters
-    parser.add_argument('--loss', type=str, default='MSELoss')
-    parser.add_argument('--model', type=str, default='SimpleCNN')
-    parser.add_argument('--optim', type=str, default='Adam')
-    parser.add_argument('--args_to_loss', action='store_true', default=False,
-                        help='whether to pass the commandline arguments to the loss function')
-
-    parser.add_argument('--resume', metavar='PATH', help='load from a path to a saved checkpoint')
-    parser.add_argument('--evaluate', action='store_true',
-                        help='evaluate model on validation set (default: false)')
-
-    # gpu/cpu
-    parser.add_argument('--gpu_num', type=int, default=0, metavar='GPU',
-                        help='choose GPU to run on.')
-
-    # CNN
-    parser.add_argument('--cnn_in_channels', type=int, default=3)
-    parser.add_argument('--cnn_hidden_channels', type=int, default=32)
-    parser.add_argument('--cnn_num_hidden_layers', type=int, default=7)
-    parser.add_argument('--interpolate', action='store_true', default=False,
-                        help='interpolate rather than learn noise as an image residual')
-    parser.add_argument('-ni', '--no_iso', action='store_true', default=False,
-                        help='not to use image ISO values as extra conditioning data')
-
-    # VGG loss
-    parser.add_argument('--vgg_feature_layer', type=int, default=11,
-                        help='VGG19 layer number from which to extract features')
-
-    args = parser.parse_args(raw_args)
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-    args.iso = not args.no_iso
-
-    if args.manual_seed is None:
-        args.manual_seed = 42
-
-    return args
-
-
-if __name__ == '__main__':
-    x = torch.randn(2, 3, 64, 64)
-    conv = GatedConvLayer(3, 32, dilation=2, conv_residual=True, preserve_size=True)
-    print(conv(x).shape)
-    # model = HybridGatedCNN(parse_arguments())
-    # model(x)
