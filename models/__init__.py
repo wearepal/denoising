9702de031863f9bc8196e3ed4b335f5771fa63fa
mb715 <mb715@sussex.ac.uk>
2018-11-19 13:27:00 +0000

    Added dilated gated model to init.

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .simple_cnn import *
 from .ircnn import *
 from .simple_complex_cnn import *
 from .gated_resnet import *
+from .dilated_gated import *
 from . import layers
 from . import complex_layers
