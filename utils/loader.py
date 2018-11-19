66ef3fbad1258d313586dacded68568b6721a259
mb715 <mb715@sussex.ac.uk>
2018-11-19 17:05:31 +0000

    deletion

--- utils/loader.py
+++ utils/loader.py
@@ -220,6 +220,6 @@ def transform_sample(sample):
         'iso': torch.FloatTensor([(sample['iso'] - 1215.32) / 958.13]),   # (x - mean) / std,
         'class': sample['class']
     }
-    print(transformed_sample['class'])
+
     return {k:v for k,v in transformed_sample.items() if v is not None}
 
