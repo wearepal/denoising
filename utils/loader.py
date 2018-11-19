16923944faf462211036d884575c13b832850b56
stoffle <stoffle@users.noreply.github.com>
2018-11-19 13:21:27 +0000

    Added class to tranform_sample

--- utils/loader.py
+++ utils/loader.py
@@ -213,6 +213,8 @@ def transform_sample(sample):
     transformed_sample = {
         'clean': clean_transforms(sample['clean']) if 'clean' in sample else None,
         'noisy': noisy_transforms(sample['noisy']),
-        'iso': torch.FloatTensor([(sample['iso'] - 1215.32) / 958.13])   # (x - mean) / std
+        'iso': torch.FloatTensor([(sample['iso'] - 1215.32) / 958.13]),   # (x - mean) / std
+        'class': sample['class']
     }
     return {k:v for k,v in transformed_sample.items() if v is not None}
+
