88de1340499eefbd31d320ebb788f1c1a4bc2b7f
mb715 <mb715@sussex.ac.uk>
2018-11-21 11:30:28 +0000

    Added GatedResNet Model. Added gated_resnet.py to init.

--- models/gated_resnet.py
+++ models/gated_resnet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from models.layers import GatedResidualBLock, GatedConvLayer
 
 
-class DGatedResNet(nn.Module):
+class GatedResNet(nn.Module):
     """
     Simple generator network with gated convolutions and a
     uniform number of filters throughout the hidden layers.
@@ -13,14 +13,14 @@ class DGatedResNet(nn.Module):
 
         # Input layer
         layers = [GatedConvLayer(args.cnn_in_channels, args.cnn_hidden_channels,
-                                 dilation=1, normalize=False, local_condition=args.iso)]
+                                 dilation=1, local_condition=args.iso)]
         # Hidden layers
-        for dilation in [2, 4, 8, 16, 1]:
+        for _ in range((args.cnn_num_hidden_layers + 1) // 2):
             layers.append(GatedResidualBLock(args.cnn_hidden_channels, args.cnn_hidden_channels,
-                                             dilation=dilation, local_condition=args.iso))
+                                             dilation=1, local_condition=args.iso))
         # Output layer
         layers.append(GatedConvLayer(args.cnn_hidden_channels, args.cnn_in_channels, dilation=1,
-                                     normalize=False, layer_activation=nn.Tanh(), local_condition=args.iso))
+                                     normalize=False, layer_activation=None, local_condition=args.iso))
         self.model = nn.ModuleList(layers)
 
         self.residual = True
@@ -32,6 +32,40 @@ class DGatedResNet(nn.Module):
             out = layer(out, c, class_labels)
 
         if self.residual:   # learn noise residual
-            out = out + x   # Should we apply tanh again after adding the residual?
+            out = out + x
+
+        return out
+
+
+class DGatedResNet(nn.Module):
+    """
+    Simple generator network with gated convolutions and a
+    uniform number of filters throughout the hidden layers.
+    """
+    def __init__(self, args):
+        super().__init__()
+
+        # Input layer
+        layers = [GatedConvLayer(args.cnn_in_channels, args.cnn_hidden_channels,
+                                 dilation=1, local_condition=args.iso)]
+        # Hidden layers
+        for dilation in [2, 4, 8, 16, 1]:
+            layers.append(GatedResidualBLock(args.cnn_hidden_channels, args.cnn_hidden_channels,
+                                             dilation=dilation, local_condition=args.iso))
+        # Output layer
+        layers.append(GatedConvLayer(args.cnn_hidden_channels, args.cnn_in_channels, dilation=1,
+                                     normalize=False, layer_activation=None, local_condition=args.iso))
+        self.model = nn.ModuleList(layers)
+
+        self.residual = True
+
+    def forward(self, x, c=None, class_labels=None):
+        out = x
+
+        for layer in self.model:
+            out = layer(out, c, class_labels)
+
+        if self.residual:   # learn noise residual
+            out = out + x
 
         return out
