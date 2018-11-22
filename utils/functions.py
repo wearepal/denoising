5245ec7001028603c72fe67705f6ce8c205f49ca
mb715 <mb715@sussex.ac.uk>
2018-11-22 16:19:57 +0000

    Formatting.

--- utils/functions.py
+++ utils/functions.py
@@ -3,8 +3,7 @@ from torch.nn.utils import spectral_norm
 
 
 def apply_spectral_norm(module):
-    has_children = hasattr(module, 'children')
-    if has_children:
+    if hasattr(module, 'children'):
         children = list(module.children())
         for i in range(len(children)):
             if isinstance(children[i], (nn.Conv2d, nn.Linear)):
