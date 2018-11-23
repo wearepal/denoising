8cbccf1b22ad927eefef4e0c042aa2aa3921c4e6
mb715 <mb715@sussex.ac.uk>
2018-11-23 11:09:48 +0000

    Fixed simple discriminator.

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 
-from models.layers import GatedConvLayer
-
+from models.layers import GatedConvLayer, ConvLayer
 
 PATCH_SIZE = 64
 
@@ -10,9 +9,8 @@ class SimpleDiscriminator(nn.Module):
     def __init__(self, args):
         super().__init__()
 
-        layers = [GatedConvLayer(args.cnn_in_channels, 64, stride=1, normalize=False,
-                                 layer_activation=nn.LeakyReLU(0.1), local_condition=args.iso,
-                                 num_classes=args.num_classes)]
+        layers = [ConvLayer(args.cnn_in_channels, 64, stride=1, normalize=False,
+                            layer_activation=nn.LeakyReLU(0.1), num_classes=args.num_classes)]
 
         in_channels = layers[0].in_channels
         in_dim = PATCH_SIZE / layers[0].stride  # patch size is 64
@@ -22,9 +20,9 @@ class SimpleDiscriminator(nn.Module):
         layer_params = [(64, 3, 2), (128, 3, 1), (128, 3, 2), (256, 3, 1), (256, 3, 2)]
 
         for out_channels, kernel_size, stride in layer_params:
-            conv = GatedConvLayer(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                                  normalize=False, layer_activation=nn.LeakyReLU(0.1),
-                                  local_condition=args.iso, num_classes=args.num_classes)
+            conv = ConvLayer(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                             normalize=False, layer_activation=nn.LeakyReLU(0.1),
+                             num_classes=args.num_classes)
             layers.append(conv)
             in_channels = out_channels
             in_dim /= stride
