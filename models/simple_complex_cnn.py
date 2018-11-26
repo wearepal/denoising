44e8920d63b3851287f14ce2ad4f0d10ce25d8a4
mb715 <mb715@sussex.ac.uk>
2018-11-26 12:26:13 +0000

    Changed dilation to 2 in hidden layers.

--- models/simple_complex_cnn.py
+++ models/simple_complex_cnn.py
@@ -20,6 +20,7 @@ class ComplexGatedCNN(nn.Module):
         # Hidden layers
         for _ in range(args.cnn_hidden_layers):
             layers.append(ComplexGatedConvLayer(num_hidden_channels, num_hidden_channels,
+                                                dilation=2, preserve_size=True,
                                                 local_condition=args.iso, num_classes=args.num_classes))
 
         layers.append(ComplexGatedConvLayer(num_hidden_channels, args.cnn_in_channels,
@@ -31,13 +32,13 @@ class ComplexGatedCNN(nn.Module):
         self.residual = not args.interpolate
 
     def forward(self, x, c=None, class_labels=None):
-        out = torch.rfft(x, onesided=False, signal_ndim=2)
+        out = torch.rfft(x, signal_ndim=2)
 
         for layer in self.model:
             out = layer(out, c, class_labels)
 
         # inverse fourier transform
-        out = torch.irfft(out, signal_ndim=2, onesided=False, signal_sizes=x.shape[2:])
+        out = torch.irfft(out, signal_ndim=2, signal_sizes=x.shape[2:])
 
         if self.residual:   # learn noise residual
             out = out + x
