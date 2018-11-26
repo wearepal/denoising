634d3ae0e2c7305e42e9739af321d1527453de88
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2018-11-26 21:13:43 +0000

    Updates to VGGLoss.

--- utils/functions.py
+++ utils/functions.py
@@ -5,7 +5,7 @@ from torch.nn.utils import spectral_norm
 
 class MeanShift(nn.Conv2d):
     def __init__(self, rgb_range, rgb_mean, rgb_std, sign=-1):
-        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        super().__init__(3, 3, kernel_size=1)
         std = torch.Tensor(rgb_std)
         self.weight.data = torch.eye(3).view(3, 3, 1, 1)
         self.weight.data.div_(std.view(3, 1, 1, 1))
