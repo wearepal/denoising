b09b3c5b47646084e4908f9e9f794b28edbbd513
mb715 <mb715@sussex.ac.uk>
2018-11-28 16:10:55 +0000

    Update init

--- models/dense_cnn.py
+++ models/dense_cnn.py
@@ -102,7 +102,6 @@ class DenseGatedCNN(nn.Module):
         def init_weights(m):
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight)
-                m.weight.data *= 0.1
             elif isinstance(m, nn.Linear):
                 m.weight.data *= 0.1
 
