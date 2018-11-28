40e68473daacb994aea15ec1b015a1417496e5c2
mb715 <mb715@sussex.ac.uk>
2018-11-28 13:18:06 +0000

    Init changes

--- models/dense_cnn.py
+++ models/dense_cnn.py
@@ -69,8 +69,8 @@ class DenseGatedCNN(nn.Module):
 
         # init
         def init_weights(m):
-            if isinstance(m, nn.Conv2d):
-                m.weight.data *= 0.1
+            if isinstance(m, nn.Parameter):
+                m.data *= 0.1
 
         self.apply(init_weights)
 
