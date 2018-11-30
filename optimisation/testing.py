f5e64aa5ced2a6ead3ba4cccccd5c8ab9391d164
stoffle <stoffle@users.noreply.github.com>
2018-11-30 20:41:16 +0000

    Fixed size missmatch in model loading for testing

--- optimisation/testing.py
+++ optimisation/testing.py
@@ -26,7 +26,7 @@ def test(args, sample_transform):
     print('==> Loading checkpoint for testing')
     checkpoint = torch.load(model_path)
     print('==> Checkpoint loaded')
-    model = getattr(models, model_args.model)(args)
+    model = getattr(models, model_args.model)(model_args)
     model = model.cuda() if args.cuda else model
     model.load_state_dict(checkpoint['model'])
     model.eval()
