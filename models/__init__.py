cb9f8b88d3992454ae7b76853c4d5e5f7fe6f08f
mb715 <mb715@sussex.ac.uk>
2019-02-14 11:57:50 +0000

    Extract fourier transform into its own class

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from .simple_cnn import *
 from .complex_cnn import *
-from .discriminator import *
 from . import layers
 from . import complex_layers
--- models/complex_cnn.py
+++ models/complex_cnn.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 
-from models import GatedConvLayer
-from models.complex_layers import ComplexGatedConvLayer
+from models.complex_layers import ComplexConvLayer
+from models.container import SequentialMeta
+from models.fft import FourierTransformerReal
 
 
-class ComplexGatedCNN(nn.Module):
+class ComplexNet(nn.Module):
     """
     Simple generator network with gated convolutions and a
     uniform number of filters throughout the hidden layers.
@@ -15,29 +16,21 @@ class ComplexGatedCNN(nn.Module):
 
         num_hidden_channels = args.cnn_hidden_channels // 2
         # Input layer
-        layers = [ComplexGatedConvLayer(args.cnn_in_channels, num_hidden_channels,
-                                        local_condition=args.iso, num_classes=args.num_classes)]
+        layers = [ComplexConvLayer(args.cnn_in_channels, num_hidden_channels, num_classes=args.num_classes)]
         # Hidden layers
         for _ in range(args.cnn_hidden_layers):
-            layers.append(ComplexGatedConvLayer(num_hidden_channels, num_hidden_channels,
-                                                local_condition=args.iso, num_classes=args.num_classes))
+            layers.append(ComplexConvLayer(num_hidden_channels, num_hidden_channels, num_classes=args.num_classes))
 
-        layers.append(ComplexGatedConvLayer(num_hidden_channels, args.cnn_in_channels,
-                                            layer_activation=None, normalize=False,
-                                            local_condition=args.iso, num_classes=args.num_classes))
+        layers.append(ComplexConvLayer(num_hidden_channels, args.cnn_in_channels, layer_activation=None, normalize=False, num_classes=args.num_classes))
 
-        self.model = nn.ModuleList(layers)
+        self.model = SequentialMeta(layers)
+        self.transformer = FourierTransformerReal(signal_dim=2)
 
         self.residual = args.residual
 
     def forward(self, x, c=None, class_labels=None):
-        out = torch.rfft(x, signal_ndim=2)
-
-        for layer in self.model:
-            out = layer(out, c, class_labels)
-
-        # inverse fourier transform
-        out = torch.irfft(out, signal_ndim=2, signal_sizes=x.shape[2:])
+        out = self.transformer.transform(x)
+        out = self.transformer.invert(self.model(out, c, class_labels))
 
         if self.residual:   # learn noise residual
             out = out + x
