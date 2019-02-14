f59fac943dc1d5d32794f0c5c1fcf64bf451faf7
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-02-14 14:06:37 +0000

    Use config file also for GAN training

--- main_gan.py
+++ main_gan.py
@@ -1,4 +1,5 @@
-import argparse
+"""Entry point for GAN models"""
+from sys import argv
 from pathlib import Path
 import random
 import shutil
@@ -12,115 +13,9 @@ from tensorboardX import SummaryWriter
 from optimisation.testing import test
 from optimisation.training import train, train_gan, validate, evaluate
 from optimisation import loss
-from utils import TransformedHuaweiDataset, transform_sample
+from utils import TransformedHuaweiDataset, transform_sample, parse_arguments
 from utils.functions import apply_spectral_norm
 import models
-
-
-def parse_arguments(raw_args=None):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-t', '--run_on_test', nargs='+',
-                        help='Evaluate a model (checkpoint) on test set. '
-                             'Args: Checkpoint path, Test data path[, Save path]')
-
-    parser.add_argument('-dd', '--data_dir', help='location of transformed data')
-    parser.add_argument('-ts', '--test_split', help='Fraction of data to be used for validation',
-                        default=0.2, type=float)
-    parser.add_argument('-ds', '--data_subset', help='Fraction of crops per image to be used',
-                        default=1.0, type=float)
-
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-
-    parser.add_argument('-nc', '--no_cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-
-    parser.add_argument('--random_seed', action="store_true",
-                        help='use random seed rather than 42.')
-
-    parser.add_argument('-sd', '--save_dir', type=str, metavar='PATH', default='',
-                        help='path to save results and checkpoints to '
-                             '(default: ../results/<model>/<current timestamp>)')
-    parser.add_argument('--num_samples_to_log', type=int, metavar='N', default=32,
-                        help='number of image samples to write to tensorboard each epoch'
-                             ' (default: 32)')
-
-    # training parameters
-    parser.add_argument('--epochs', default=100, type=int, metavar='N',
-                        help='number of total epochs to run (default: 100)')
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='manual epoch number (useful on restarts)')
-    parser.add_argument('--pretrain_epochs', default=0, type=int, metavar='N',
-                        help='number of epochs to pre-train the generator for (default: 0)')
-
-    parser.add_argument('-trb', '--train_batch_size', default=256, type=int,
-                        metavar='N', help='mini-batch size for training data (default: 256)')
-    parser.add_argument('-teb', '--test_batch_size', default=256, type=int,
-                        metavar='N', help='mini-batch size for test data (default: 256)')
-
-    # optimization
-    parser.add_argument('--betas', type=tuple, default=(0, 0.9),
-                        help='beta values for Adam optimizer (default: (0, 0.9)')
-    parser.add_argument('-lrg', '--gen_learning_rate', default=1e-4, type=float,
-                        metavar='LR', help='initial learning rate (default: 1e-4)')
-    parser.add_argument('-lrd', '--disc_learning_rate', default=4e-4, type=float,
-                        metavar='LR', help='initial learning rate (default: 4e-4)')
-    parser.add_argument('--disc_iters', type=int, default=1,
-                        help='number of discriminator steps per generator step (default: 1)')
-
-    # loss
-    parser.add_argument('--content_loss', type=str, default='MSELoss',
-                        help='content loss for the generator')
-    parser.add_argument('--adv_loss', type=str, default='HingeLossGAN', help='adversarial loss')
-    parser.add_argument('--adv_weight', type=float, default=1e-3,
-                        help='weight to place on adversarial loss (default: 1e-3)')
-
-    # model parameters
-    parser.add_argument('-gen', '--generator', type=str, default='GatedCNN',
-                        help='model to use as the generator')
-    parser.add_argument('-disc', '--discriminator', type=str, default='SimpleDiscriminator',
-                        help='model to use as the discriminator')
-    parser.add_argument('--args_to_loss', action='store_true', default=False,
-                        help='whether to pass the commandline arguments to the loss function')
-
-    parser.add_argument('--resume', metavar='PATH', help='load from a path to a saved checkpoint')
-    parser.add_argument('--evaluate', action='store_true',
-                        help='evaluate model on validation set (default: false)')
-
-    # gpu/cpu
-    parser.add_argument('--gpu_num', type=int, default=0, metavar='GPU',
-                        help='choose GPU to run on.')
-
-    # CNN
-    parser.add_argument('--cnn_in_channels', type=int, default=3)
-    parser.add_argument('--cnn_hidden_channels', type=int, default=32)
-    parser.add_argument('--cnn_hidden_layers', type=int, default=7)
-
-    parser.add_argument('--disc_hidden_channels', type=int, default=128)
-
-    parser.add_argument('--interpolate', action='store_true', default=False,
-                        help='interpolate rather than learn noise as an image residual')
-    parser.add_argument('-ni', '--no_iso', action='store_true', default=False,
-                        help='not to use image ISO values as extra conditioning data')
-    parser.add_argument('--use_class', action='store_true', default=False,
-                        help='use the class information of images')
-
-    # VGG loss
-    parser.add_argument('--vgg_feature_layer', type=str, default='22',
-                        choices=('22', '54'), help='VGG19 layer number from which to extract features')
-
-    args = parser.parse_args(raw_args)
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-    args.iso = not args.no_iso
-    args.num_classes = 3 if args.use_class else 0
-
-    if args.random_seed:
-        args.seed = random.randint(1, 100000)
-    else:
-        args.seed = 42
-
-    return args
 
 
 def main(args):
@@ -132,7 +27,7 @@ def main(args):
         # gpu device number
         torch.cuda.set_device(args.gpu_num)
 
-    if args.run_on_test:
+    if args.test_data_dir:
         test(args, transform_sample)
         return
 
@@ -146,7 +41,7 @@ def main(args):
 
     kwargs = {'pin_memory': True} if args.cuda else {}
 
-    print('\nMODEL SETTINGS: \n', args, '\n')
+    print('\nMODEL SETTINGS: \n', args.state_dict(), '\n')
     print("Random Seed: ", args.seed)
 
     # Save config
@@ -164,9 +59,9 @@ def main(args):
     discriminator = discriminator.cuda() if args.cuda else discriminator
 
     gen_optimizer = torch.optim.Adam(generator.parameters(), lr=args.gen_learning_rate,
-                                     betas=args.betas)
+                                     betas=(args.beta1, args.beta2))
     disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=args.disc_learning_rate,
-                                      betas=args.betas)
+                                      betas=(args.beta1, args.beta2))
 
     criterion_constructor = getattr(loss, args.content_loss)
     content_criterion = criterion_constructor(args) if args.args_to_loss else criterion_constructor()
@@ -253,4 +148,4 @@ def save_checkpoint(checkpoint, filename, is_best, save_path):
 
 
 if __name__ == '__main__':
-    main(parse_arguments())
+    main(parse_arguments(argv[1] if len(argv) >= 2 else "run_configs/default_gan.ini"))
--- utils/config.py
+++ utils/config.py
@@ -13,27 +13,36 @@ class Settings:
 
     def set_int(self, section, name):
         """Set integer configuration value from `section`"""
-        self.__set(name, section.getint(name))
+        try:
+            self.__set(name, section.getint(name))
+        except ValueError:
+            raise ValueError(f"Config file: the value for \"{name}\" has to be of type int.")
 
     def set_float(self, section, name):
         """Set float configuration value from `section`"""
-        self.__set(name, section.getfloat(name))
+        try:
+            self.__set(name, section.getfloat(name))
+        except ValueError:
+            raise ValueError(f"Config file: the value for \"{name}\" has to be of type float.")
 
     def set_bool(self, section, name):
         """Set boolean configuration value from `section`"""
-        self.__set(name, section.getboolean(name))
+        try:
+            self.__set(name, section.getboolean(name))
+        except ValueError:
+            raise ValueError(f"Config file: the value for \"{name}\" has to be of type boolean.")
 
     def __set(self, name, value):
         self._key_list.append(name)
         self.__setattr__(name, value)
         if value is None:
-            print(f"Warning: no value specified for \"{name}\" (set to None).")
+            print(f"Warning: no value specified for \"{name}\" (value is therefore set to None).")
 
-    def to_dict(self):
+    def state_dict(self):
         """Convert this settings object to a dictionary"""
         return {name: getattr(self, name) for name in self._key_list}
 
-    def from_dict(self, dictionary):
+    def load_state_dict(self, dictionary):
         """Load settings from a dictionary"""
         for key, value in dictionary.iter():
             self.__setattr__(key, value)
@@ -41,14 +50,13 @@ class Settings:
 
 def parse_arguments(config_file):
     """
-    This function basically just checks if all config values are there
-    and if they have the right type.
+    This function basically just checks if all config values have the right type.
     """
     args = Settings()
     parser = configparser.ConfigParser()
     parser.read(config_file)
 
-    general_config = parser['general']
+    general_config = get_section(parser, 'general')
     args.set_str(general_config, 'test_data_dir')
     args.set_str(general_config, 'results_dir')
     args.set_str(general_config, 'data_dir')
@@ -59,30 +67,47 @@ def parse_arguments(config_file):
     args.set_bool(general_config, 'random_seed')
     args.set_str(general_config, 'save_dir')
     args.set_str(general_config, 'num_samples_to_log')
+    args.set_str(general_config, 'resume')
+    args.set_bool(general_config, 'evaluate')
 
     # training parameters
-    training_config = parser['training']
+    training_config = get_section(parser, 'training')
     args.set_int(training_config, 'epochs')
     args.set_int(training_config, 'start_epoch')
+    args.set_int(training_config, 'pretrain_epochs')
     args.set_int(training_config, 'train_batch_size')
     args.set_int(training_config, 'test_batch_size')
     args.set_float(training_config, 'learning_rate')
 
+    # optimization parameters
+    optimization_config = get_section(parser, 'optimization')
+    args.set_float(optimization_config, 'beta1')
+    args.set_float(optimization_config, 'beta2')
+    args.set_float(optimization_config, 'gen_learning_rate')
+    args.set_float(optimization_config, 'disc_learning_rate')
+    args.set_int(optimization_config, 'disc_iters')
+
+    # loss parameters
+    loss_config = get_section(parser, 'loss')
+    args.set_str(loss_config, 'loss')
+    args.set_str(loss_config, 'content_loss')
+    args.set_str(loss_config, 'adv_loss')
+    args.set_float(loss_config, 'adv_weight')
+    args.set_bool(loss_config, 'args_to_loss')
+
     # model parameters
-    model_config = parser['model']
-    args.set_str(model_config, 'loss')
+    model_config = get_section(parser, 'model')
     args.set_str(model_config, 'model')
+    args.set_str(model_config, 'generator')
+    args.set_str(model_config, 'discriminator')
     args.set_str(model_config, 'optim')
-    args.set_bool(model_config, 'args_to_loss')
-    args.set_str(model_config, 'resume')
-    args.set_bool(model_config, 'evaluate')
 
     # gpu/cpu
-    gpu_config = parser['GPU']
+    gpu_config = get_section(parser, 'GPU')
     args.set_int(gpu_config, 'gpu_num')
 
     # CNN
-    cnn_config = parser['CNN']
+    cnn_config = get_section(parser, 'CNN')
     args.set_int(cnn_config, 'cnn_in_channels')
     args.set_int(cnn_config, 'cnn_hidden_channels')
     args.set_int(cnn_config, 'cnn_hidden_layers')
@@ -91,7 +116,7 @@ def parse_arguments(config_file):
     args.set_bool(cnn_config, 'use_class')
 
     # VGG loss
-    vgg_config = parser['VGG']
+    vgg_config = get_section(parser, 'VGG')
     args.set_str(vgg_config, 'vgg_feature_layer')
 
     args.num_classes = 3 if args.use_class else 0
@@ -102,3 +127,10 @@ def parse_arguments(config_file):
         args.seed = 42
 
     return args
+
+
+def get_section(parser, name):
+    """Get section of the config parser. Creates section if it doesn't exist"""
+    if not parser.has_section(name):
+        parser.add_section(name)
+    return parser[name]
