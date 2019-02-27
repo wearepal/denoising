0801c9c29a2fa597ed89251b90a58f92c726387b
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-02-27 14:19:34 +0000

    Improve config parser

--- utils/config.py
+++ utils/config.py
@@ -1,47 +1,52 @@
 """Configuration loading and parsing"""
 import configparser
+
 import torch
 
 
 class Settings:
     """Object that holds the configuration"""
-    def __init__(self):
+    def __init__(self, config_file):
+        self.parser = configparser.ConfigParser()
+        self.parser.read(config_file)
         self._key_list = []
 
-    def set_str(self, section, name):
+    def set_str(self, section, key):
         """Set string configuration value from `section`"""
-        self.__set(name, section.get(name))
+        self.__set(self.__section(section).get, key, "string")
 
-    def set_int(self, section, name):
+    def set_int(self, section, key):
         """Set integer configuration value from `section`"""
-        try:
-            self.__set(name, section.getint(name))
-        except ValueError:
-            raise ValueError(f"Config file: the value for \"{name}\" has to be of type int.")
+        self.__set(self.__section(section).getint, key, "int")
 
-    def set_float(self, section, name):
+    def set_float(self, section, key):
         """Set float configuration value from `section`"""
-        try:
-            self.__set(name, section.getfloat(name))
-        except ValueError:
-            raise ValueError(f"Config file: the value for \"{name}\" has to be of type float.")
+        self.__set(self.__section(section).getfloat, key, "float")
 
-    def set_bool(self, section, name):
+    def set_bool(self, section, key):
         """Set boolean configuration value from `section`"""
-        try:
-            self.__set(name, section.getboolean(name))
-        except ValueError:
-            raise ValueError(f"Config file: the value for \"{name}\" has to be of type boolean.")
+        self.__set(self.__section(section).getboolean, key, "boolean")
 
-    def __set(self, name, value):
-        self._key_list.append(name)
-        self.__setattr__(name, value)
-        if value is None:
-            print(f"Warning: no value specified for \"{name}\" (value is therefore set to None).")
+    def __section(self, section_name):
+        """Find section"""
+        for section in self.parser.sections():
+            if section.lower() == section_name.lower():
+                return self.parser[section]
+        self.parser.add_section(section_name)
+        return self.parser[section_name]
+
+    def __set(self, getter, key, type_name):
+        """Set a value from the getter where the getter is a function"""
+        try:
+            value = getter(key)
+        except ValueError:
+            raise ValueError(f"Config file: the value for \"{key}\" has to be of type {type_name}.")
+        self._key_list.append(key)
+        self.__setattr__(key, value)
 
     def state_dict(self):
         """Convert this settings object to a dictionary"""
-        return {name: getattr(self, name) for name in self._key_list}
+        return {key: getattr(self, key) for key in self._key_list}
 
     def load_state_dict(self, dictionary):
         """Load settings from a dictionary"""
@@ -53,74 +58,64 @@ def parse_arguments(config_file):
     """
     This function basically just checks if all config values have the right type.
     """
-    args = Settings()
-    parser = configparser.ConfigParser()
-    parser.read(config_file)
+    args = Settings(config_file)
 
-    general_config = get_section(parser, 'general')
-    args.set_str(general_config, 'test_data_dir')
-    args.set_str(general_config, 'results_dir')
-    args.set_str(general_config, 'data_dir')
-    args.set_float(general_config, 'test_split')
-    args.set_float(general_config, 'data_subset')
-    args.set_int(general_config, 'workers')
-    args.set_bool(general_config, 'cuda')
-    args.set_bool(general_config, 'random_seed')
-    args.set_str(general_config, 'save_dir')
-    args.set_str(general_config, 'num_samples_to_log')
-    args.set_str(general_config, 'resume')
-    args.set_bool(general_config, 'evaluate')
+    args.set_str('general', 'test_data_dir')
+    args.set_str('general', 'results_dir')
+    args.set_str('general', 'data_dir')
+    args.set_float('general', 'test_split')
+    args.set_float('general', 'data_subset')
+    args.set_int('general', 'workers')
+    args.set_bool('general', 'cuda')
+    args.set_bool('general', 'random_seed')
+    args.set_str('general', 'save_dir')
+    args.set_str('general', 'num_samples_to_log')
+    args.set_str('general', 'resume')
+    args.set_bool('general', 'evaluate')
 
     # training parameters
-    training_config = get_section(parser, 'training')
-    args.set_int(training_config, 'epochs')
-    args.set_int(training_config, 'start_epoch')
-    args.set_int(training_config, 'pretrain_epochs')
-    args.set_int(training_config, 'train_batch_size')
-    args.set_int(training_config, 'test_batch_size')
-    args.set_float(training_config, 'learning_rate')
+    args.set_int('training', 'epochs')
+    args.set_int('training', 'start_epoch')
+    args.set_int('training', 'pretrain_epochs')
+    args.set_int('training', 'train_batch_size')
+    args.set_int('training', 'test_batch_size')
+    args.set_float('training', 'learning_rate')
 
     # optimization parameters
-    optimization_config = get_section(parser, 'optimization')
-    args.set_float(optimization_config, 'beta1')
-    args.set_float(optimization_config, 'beta2')
-    args.set_float(optimization_config, 'gen_learning_rate')
-    args.set_float(optimization_config, 'disc_learning_rate')
-    args.set_int(optimization_config, 'disc_iters')
+    args.set_float('optimization', 'beta1')
+    args.set_float('optimization', 'beta2')
+    args.set_float('optimization', 'gen_learning_rate')
+    args.set_float('optimization', 'disc_learning_rate')
+    args.set_int('optimization', 'disc_iters')
 
     # loss parameters
-    loss_config = get_section(parser, 'loss')
-    args.set_str(loss_config, 'loss')
-    args.set_str(loss_config, 'content_loss')
-    args.set_str(loss_config, 'adv_loss')
-    args.set_float(loss_config, 'adv_weight')
-    args.set_bool(loss_config, 'args_to_loss')
+    args.set_str('loss', 'loss')
+    args.set_str('loss', 'content_loss')
+    args.set_str('loss', 'adv_loss')
+    args.set_float('loss', 'adv_weight')
+    args.set_bool('loss', 'args_to_loss')
 
     # model parameters
-    model_config = get_section(parser, 'model')
-    args.set_str(model_config, 'model')
-    args.set_str(model_config, 'generator')
-    args.set_str(model_config, 'discriminator')
-    args.set_str(model_config, 'optim')
+    args.set_str('model', 'model')
+    args.set_str('model', 'generator')
+    args.set_str('model', 'discriminator')
+    args.set_str('model', 'optim')
 
     # gpu/cpu
-    gpu_config = get_section(parser, 'GPU')
-    args.set_int(gpu_config, 'gpu_num')
-    args.set_bool(gpu_config, 'multi_gpu')
+    args.set_int('gpu', 'gpu_num')
+    args.set_bool('gpu', 'multi_gpu')
 
     # CNN
-    cnn_config = get_section(parser, 'CNN')
-    args.set_int(cnn_config, 'cnn_in_channels')
-    args.set_int(cnn_config, 'cnn_hidden_channels')
-    args.set_int(cnn_config, 'cnn_hidden_layers')
-    args.set_bool(cnn_config, 'residual')
-    args.set_bool(cnn_config, 'iso')
-    args.set_bool(cnn_config, 'use_class')
-    args.set_bool(cnn_config, 'learn_beta')
+    args.set_int('cnn', 'cnn_in_channels')
+    args.set_int('cnn', 'cnn_hidden_channels')
+    args.set_int('cnn', 'cnn_hidden_layers')
+    args.set_bool('cnn', 'residual')
+    args.set_bool('cnn', 'iso')
+    args.set_bool('cnn', 'use_class')
+    args.set_bool('cnn', 'learn_beta')
 
     # VGG loss
-    vgg_config = get_section(parser, 'VGG')
-    args.set_str(vgg_config, 'vgg_feature_layer')
+    args.set_str('vgg', 'vgg_feature_layer')
 
     args.num_classes = 3 if args.use_class else 0
     args.cuda = args.cuda and torch.cuda.is_available()
@@ -131,10 +126,3 @@ def parse_arguments(config_file):
         args.seed = 42
 
     return args
-
-
-def get_section(parser, name):
-    """Get section of the config parser. Creates section if it doesn't exist"""
-    if not parser.has_section(name):
-        parser.add_section(name)
-    return parser[name]
